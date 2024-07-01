f7e7af9a6f0f6d0c8bd77dc1cdc748cac3ed8677
rifahad <rifahadmt202@gmail.com>
2024-07-02 02:33:49 +0530

    basic needed components added

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,32 @@ import { Footer } from "@/components/Footer";
 import { GlobeDemo } from "@/components/GlobeDemo";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { ParallexEffect } from "@/components/ParallexEffect";
+import ParticlesDemo from "@/components/ParticlesDemo";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <main className="relative bg-black text-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full ">
-        <FloatingNav navItems={[{name: 'Home', link: "/", icon: <FaHome/> },{name: 'Skills', link: "/skills", icon: <FaHome/> },{name: 'Education', link: "/", icon: <FaHome/> },{name: 'Experiances', link: "/", icon: <FaHome/> }]} />
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          { name: 'Home', link: "/", icon: <FaHome /> },
+          { name: 'Skills', link: "/", icon: <FaHome /> },
+          { name: 'Education', link: "/", icon: <FaHome /> },
+          { name: 'Experiences', link: "/", icon: <FaHome /> }
+        ]} />
         <Hero />
-        <VortexDemo />  
+        <GlobeDemo />
+        <ParallexEffect />
         <Footer />
         <Grid />
-        <GlobeDemo />
+        <ParticlesDemo />
+        <VortexDemo />
+        <img src="/GifImages/Robo2.gif" alt="Robo" className="w-[100px] fixed bottom-10 right-5" />
       </div>
     </main>
   );
 }
+
+export default Home;
--- components/ParallexEffect.tsx
+++ components/ParallexEffect.tsx
@@ -1,52 +1,16 @@
-export const projects = [
-    {
-      title: "E-Commerce",
-      description:
-        "A E-Commerce Website for selling things...",
-      link: "https://github.com/Rifahad/Asterisk-E-Commerce-Website",
-    },
-    {
-      title: "Ridrocks Gamers Community",
-      description:
-        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-      link: "https://github.com/Rifahad/RIGDROCKS-GAMERS-COMMUNITY-PLATFORM-USING-MERN",
-    },
-    {
-      title: "AI Application React Native",
-      description:
-        "A multinational technology company that specializes in Internet-related services and products.",
-      link: "https://google.com",
-    },
-    {
-      title: "Scrap Collector Frontend",
-      description:
-        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-      link: "https://meta.com",
-    },
-    {
-        title: "Scrap Collector Backend",
-        description:
-          "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-        link: "https://meta.com",
-      },
-      {
-        title: "FlickFusion Movie Application", 
-        description:
-          "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-        link: "https://meta.com",
-      },
-  
+"use client";
+import React from "react";
+import { HeroParallax } from "./ui/hero-parallax";
 
-  ];
-
-
-  
-export const ParallexDatas = [
+export function ParallexEffect() {
+  return <HeroParallax products={products} />;
+}
+export const products = [
   {
     title: "Moonbeam",
     link: "https://gomoonbeam.com",
     thumbnail:
-      "https://tse1.mm.bing.net/th?id=OIP.PYipJ_hSncugM2SwnZitvgHaEK&pid=Api&rs=1&c=1&qlt=95&w=176&h=98",
+      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
   },
   {
     title: "Cursor",
